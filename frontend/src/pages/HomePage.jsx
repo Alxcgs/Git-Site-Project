ed603eae557893a28876dbcf0d0251cd53217d23
Alxcgs <gerasymchuk.os@gmail.com>
2024-11-15 18:01:44 +0200

    Huge start Create pages and half of all frontend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+const HomePage = () => {
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
---
+++ frontend/src/pages/LikesPage.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+
+const LikesPage = () => {
+  return (
+    <div>LikesPage</div>
+  )
+}
+
+export default LikesPage
