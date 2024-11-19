7dcc151c5919dad8a37abfc42da66ff8e8ef11a2
Alxcgs <gerasymchuk.os@gmail.com>
2024-11-19 19:07:51 +0200

    "Updated backend and frontend code, added passport and express-session dependencies, and modified various files including server.js, App.jsx, and vite.config.js."

--- React/backend/server.js
+++ React/backend/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+
+import "./passport/github.auth.js";
 
 import userRoutes from "./routes/user.route.js";
 import exploreRoutes from "./routes/explore.route.js";
+import authRoutes from "./routes/auth.route.js";
+
+import connectMongoDb from "./db/connectMongoDb.js";
 
 
 dotenv.config();
 
 const app = express();
+
+app.use(session({secret: "keyboard cat", resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 
@@ -16,10 +27,12 @@ app.get("/", (req, res) => {
     res.send("Server is running!");
 });
 
+app.use("/api/auth", authRoutes);
 app.use("/api/users" , userRoutes);
 app.use("/api/explore", exploreRoutes);
 
 
 app.listen(4000, () => {
     console.log("Server is running on http://localhost:4000");
+    connectMongoDb();
 });
