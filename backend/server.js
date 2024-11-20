5f7efb988fa7504560f74fb92895710b4fbf07f7
Alxcgs <gerasymchuk.os@gmail.com>
2024-11-20 02:34:33 +0200

    "Updated GitHub OAuth callback URL and server port in backend code"

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ import connectMongoDB from "./db/connectMongoDb.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 console.log("dirname",__dirname);
